b4aa51109c8583522d3415ca72e3f503eebca800
BosayaIS <rinakoner65@gmail.com>
2022-06-09 15:35:33 +0300

    lesson_4

--- lesson_3/Z_3.py
+++ lesson_3/Z_3.py
@@ -1,16 +1,20 @@
 """
 3. В массиве случайных целых чисел поменять местами минимальный и максимальный элементы.
 """
-import random
+import random, timeit, cProfile
 data = [random.randint(1, 100) for i in range(10)]
-max = 0
-min = 100
-print(data)
-for i in data:
-    max = i if max < i else max
-    min = i if min > i else min
-spam = data.index(min)
-data[data.index(max)] = min
-data[spam] = max
-print(data)
+#print(data)
+def main(max, min):
+    for i in data:
+        max = i if max < i else max
+        min = i if min > i else min
+    spam = data.index(min)
+    data[data.index(max)] = min
+    data[spam] = max
+    return data
+#print(main(0, 100))
 
+#cProfile.run('main(0, 100)')
+#1    0.000    0.000    0.000    0.000 Z_3.py:7(main)
+# "Z_3.main(0,100)"
+# 1000 loops, best of 5: 671 nsec per loop
--- lesson_3/Z_5.py
+++ lesson_3/Z_5.py
@@ -2,11 +2,25 @@
 5. В массиве найти максимальный отрицательный элемент. Вывести на экран его значение и позицию в массиве.
 Примечание к задаче: пожалуйста не путайте «минимальный» и «максимальный отрицательный». Это два абсолютно разных значения.
 """
-import random
-data = [random.randint(-100, 100) for i in range(20)]
-max = -100
-print(data)
-for i in data:
-    if i < 0:
-        max = i if max < i else max
-print(max)
+import random, timeit, cProfile
+def main(size):
+    data = [random.randint(-100, 100) for i in range(size)]
+    max = -100
+#    print(data)
+    for i in data:
+        if i < 0:
+            max = i if max < i else max
+#    print(f'Максимальный отрицательный элемент {max} находится на {data.index(max)} позиции')
+    return max, data.index(max)
+
+#cProfile.run('main(20)')
+# 1    0.000    0.000    0.000    0.000 Z_5.py:6(main)
+# 1    0.000    0.000    0.000    0.000 Z_5.py:7(<listcomp>)
+# "Z_5.main(20)"
+# 1000 loops, best of 5: 12.4 usec per loop
+# "Z_5.main(50)"
+# 1000 loops, best of 5: 30 usec per loop
+# "Z_5.main(100)"
+# 1000 loops, best of 5: 59.5 usec per loop
+
+
